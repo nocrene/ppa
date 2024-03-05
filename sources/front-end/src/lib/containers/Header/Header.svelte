a346419ec5e2c35d87858406e6a13e1f2562da25
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2024-03-05 23:06:17 +0100

    added Header

--- sources/front-end/src/lib/containers/Header/Header.svelte
+++ sources/front-end/src/lib/containers/Header/Header.svelte
@@ -1,1 +1,35 @@
-header
+<style>
+  header {
+    display: grid;
+    grid-template-areas:
+      'site-title'
+      'nav'
+    ;
+    justify-content: center;
+    align-items: center;
+
+    & h1 {
+      grid-area: site-title;
+      color: var(--theme-black);
+    }
+
+    & nav {
+      grid-area: nav;
+      display: grid;
+      grid-auto-flow: column;
+      gap: var(--main-grid-gap);
+
+      & :is(a, a:visited) {
+        color: var(--theme-orange);
+      }
+    }
+  }
+</style>
+
+<header>
+  <h1>ppa</h1>
+  <nav>
+    <a href="/sites">sites</a>
+    <a href="/settings">settings</a>
+  </nav>
+</header>
