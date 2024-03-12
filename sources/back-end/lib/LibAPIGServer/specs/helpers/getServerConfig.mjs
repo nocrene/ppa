7747bc63c1195c29c02d8ce30d0622c712a62139
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2024-03-12 19:35:45 +0100

    fix: getServerConfig

--- sources/back-end/lib/LibAPIGServer/specs/helpers/getServerConfig.mjs
+++ sources/back-end/lib/LibAPIGServer/specs/helpers/getServerConfig.mjs
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-export const getServerConfig = (debuglog = () => {}) => Object.freeze({
+export const getServerConfig = () => Object.freeze({
   server: {
     host: process.env.WS_HOST,
     port: parseInt(process.env.WS_PORT, 10),
